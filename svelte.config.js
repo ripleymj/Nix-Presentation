f098f79b3a61f58b9e076f3b2cecfbcc69d2ac9c
Mike Ripley <ripleymj@jmu.edu>
2025-10-16 06:54:10 -0400

    v5

--- svelte.config.js
+++ svelte.config.js
@@ -6,3 +6,19 @@ export default {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
 }
+
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
+  }
+};
+
+export default config;
