8d1b0927fc6c9fe6af62272d3ed4ab2a4e442f07
Mike Ripley <ripleymj@jmu.edu>
2025-10-16 07:08:59 -0400

    v9

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       fallback: '404.html'
@@ -13,9 +15,4 @@ const config = {
   }
 };
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default config {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+export default config;
