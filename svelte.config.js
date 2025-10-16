83b13a2e69ced3f4ea50e408e5c86a4e34668d2e
Mike Ripley <ripleymj@jmu.edu>
2025-10-16 10:09:29 -0400

    v17

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
     adapter: adapter({
       pages: 'dist',
       assets: 'dist',
-      fallback: '404.html'
+      fallback: null,
+      precompress: false,
+      strict: true
     }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
